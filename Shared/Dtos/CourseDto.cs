2b35699fa7e2cdc0b977eb8dc0d5ce5b174d9926
pontusalm <pontus.alm@gmail.com>
2023-03-07 16:20:43 +0100

    CourseDto fixed

--- Shared/Dtos/CourseDto.cs
+++ Shared/Dtos/CourseDto.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LexiconLMSBlazor.Server.Models
+namespace LexiconLMSBlazor.Shared.Dtos
 {
     public class Course
     {
