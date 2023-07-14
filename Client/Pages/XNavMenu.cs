8b05178bd0db4656ba17856d158a75121b717c32
Björn Lindqvist <bi-lqvist@live.se>
2023-07-14 19:15:29 +0200

    XNavMeny updated and optimized!

--- Client/Pages/XNavMenu.cs
+++ Client/Pages/XNavMenu.cs
@@ -1,36 +1,48 @@
-﻿public class XNavMenu // Klass som kan kommunicera med navmenyn.
+﻿using Microsoft.AspNetCore.Components.Web;
+
+public class XNavMenu // Klass som kan kommunicera med navmenyn.
 {
     public bool JohnDoe { get; private set; } = true;
 
-    public string classInfoMess { get; private set; } = "d-none";
+    public string ClassInfoMess { get; private set; } = "d-none";
 
     public string InfoMess { get; private set; } = string.Empty;
 
-    public string classMessMess { get; private set; } = "d-none";
+    public string ClassMessMess { get; private set; } = "d-none";
 
     public string MessMess { get; private set; } = string.Empty;
 
-    public string classDoneMess { get; private set; } = "d-none";
+    public string ClassDoneMess { get; private set; } = "d-none";
 
     public string DoneMess { get; private set; } = string.Empty;
 
-    public string classErrorMess { get; private set; } = "d-none";
+    public string ClassErrorMess { get; private set; } = "d-none";
 
     public string ErrorMess { get; private set; } = string.Empty;
 
-    public string classAvatarMess { get; private set; } = "d-none"; // Class for avatar.
+    public string ClassAvatarMess { get; private set; } = "d-none"; // Class for avatar.
 
     public string PathImgMess { get; set; } = string.Empty; // Path to avatar.
 
-    public string classAvatarLog { get; private set; } = "d-none"; // Class for log avatar.
+    public string ClassAvatarLog { get; private set; } = "d-none"; // Class for log avatar.
 
     public string PathImgLog { get; set; } = string.Empty; // Path to log avatar.
 
-    public string? classReg { get; set; } // show or hide.
+    public string? ClassReg { get; set; } // show or hide.
 
-    public string? classRegStatus { get; set; } // regon or regoff.
+    public string? ClassRegStatus { get; set; } // regon or regoff.
 
     public string? RegText { get; set; } // REG ON or REG OFF.
+
+    public int Pos_H { get; set; } // Moveable Toaster.
+
+    public int Pos_V { get; set; } // Moveable Toaster.
+
+    private static bool IsDown; // Moveable Toaster.
+
+    private static int Diff_X; // Moveable Toaster.
+
+    private static int Diff_Y; // Moveable Toaster.
 
     public event Action? OnChange;
 
@@ -42,28 +54,28 @@
 
     public void SetInfo(string classx, string info)
     {
-        classInfoMess = classx;
+        ClassInfoMess = classx;
         InfoMess = info;
         NotifyStateChanged();
     }
 
     public void SetMess(string classx, string mess)
     {
-        classMessMess = classx;
+        ClassMessMess = classx;
         MessMess = mess;
         NotifyStateChanged();
     }
 
     public void SetDone(string classx, string done)
     {
-        classDoneMess = classx;
+        ClassDoneMess = classx;
         DoneMess = done;
         NotifyStateChanged();
     }
 
     public void SetError(string classx, string error)
     {
-        classErrorMess = classx;
+        ClassErrorMess = classx;
         ErrorMess = error;
         NotifyStateChanged();
     }
@@ -72,12 +84,12 @@
     {
         if (onoff)
         {
-            classRegStatus = "regon";
+            ClassRegStatus = "regon";
             RegText = "REG ON";
         }
         else
         {
-            classRegStatus = "regoff";
+            ClassRegStatus = "regoff";
             RegText = "REG OFF";
         }
         NotifyStateChanged();
@@ -90,10 +102,10 @@
             case 'i':
             case 'I':
                 {
-                    classMessMess = "d-none";
-                    classDoneMess = "d-none";
-                    classErrorMess = "d-none";
-                    classAvatarMess = "d-none";
+                    ClassMessMess = "d-none";
+                    ClassDoneMess = "d-none";
+                    ClassErrorMess = "d-none";
+                    ClassAvatarMess = "d-none";
                     MessMess = string.Empty;
                     DoneMess = string.Empty;
                     ErrorMess = string.Empty;
@@ -103,10 +115,10 @@
             case 'm':
             case 'M':
                 {
-                    classInfoMess = "d-none";
-                    classDoneMess = "d-none";
-                    classErrorMess = "d-none";
-                    classAvatarMess = "d-none";
+                    ClassInfoMess = "d-none";
+                    ClassDoneMess = "d-none";
+                    ClassErrorMess = "d-none";
+                    ClassAvatarMess = "d-none";
                     InfoMess = string.Empty;
                     DoneMess = string.Empty;
                     ErrorMess = string.Empty;
@@ -116,10 +128,10 @@
             case 'd':
             case 'D':
                 {
-                    classInfoMess = "d-none";
-                    classMessMess = "d-none";
-                    classErrorMess = "d-none";
-                    classAvatarMess = "d-none";
+                    ClassInfoMess = "d-none";
+                    ClassMessMess = "d-none";
+                    ClassErrorMess = "d-none";
+                    ClassAvatarMess = "d-none";
                     InfoMess = string.Empty;
                     MessMess = string.Empty;
                     ErrorMess = string.Empty;
@@ -129,10 +141,10 @@
             case 'e':
             case 'E':
                 {
-                    classInfoMess = "d-none";
-                    classMessMess = "d-none";
-                    classDoneMess = "d-none";
-                    classAvatarMess = "d-none";
+                    ClassInfoMess = "d-none";
+                    ClassMessMess = "d-none";
+                    ClassDoneMess = "d-none";
+                    ClassAvatarMess = "d-none";
                     InfoMess = string.Empty;
                     MessMess = string.Empty;
                     DoneMess = string.Empty;
@@ -142,10 +154,10 @@
             case 'v':
             case 'V':
                 {
-                    classInfoMess = "d-none";
-                    classMessMess = "d-none";
-                    classDoneMess = "d-none";
-                    classErrorMess = "d-none";
+                    ClassInfoMess = "d-none";
+                    ClassMessMess = "d-none";
+                    ClassDoneMess = "d-none";
+                    ClassErrorMess = "d-none";
                     InfoMess = string.Empty;
                     MessMess = string.Empty;
                     DoneMess = string.Empty;
@@ -154,11 +166,11 @@
                 }
             default:
                 {
-                    classInfoMess = "d-none";
-                    classMessMess = "d-none";
-                    classDoneMess = "d-none";
-                    classErrorMess = "d-none";
-                    classAvatarMess = "d-none";
+                    ClassInfoMess = "d-none";
+                    ClassMessMess = "d-none";
+                    ClassDoneMess = "d-none";
+                    ClassErrorMess = "d-none";
+                    ClassAvatarMess = "d-none";
                     InfoMess = string.Empty;
                     MessMess = string.Empty;
                     DoneMess = string.Empty;
@@ -172,17 +184,50 @@
 
     public void SetLogAvatar(string classx, string path)
     {
-        classAvatarLog = classx;
+        ClassAvatarLog = classx;
         PathImgLog = path;
         NotifyStateChanged();
     }
 
     public void SetMessAvatar(string classx, string path)
     {
-        classAvatarMess = classx;
+        ClassAvatarMess = classx;
         PathImgMess = path;
         NotifyStateChanged();
     }
 
     private void NotifyStateChanged() => OnChange?.Invoke();
+
+    // ---------------------------------------------------------------
+    // Här har jag lagt andra funktioner som förekommer mer än en gång:
+    // ---------------------------------------------------------------
+
+    public static string StartEndDate(DateTime start, DateTime end) // Returnerar datumsträng.
+    {
+        return $"{start.ToString()[..10]} | {end.ToString()[..10]}";
+    }
+
+    public async Task Intermission(int time, bool hide) // Paus.
+    {
+        if (hide) SetReset('a');
+        await Task.Delay(time);
+    }
+
+    public void Mouse(int height, char act, MouseEventArgs e) // Moveable Toaster.
+    {
+        if (!IsDown && act == 'D') // Skillnaden mellan muspekarens position och css-värdet av objektet.
+        {
+            Diff_X = (int)e.ClientX - Pos_H;
+            Diff_Y = height - (Pos_V + (int)e.ClientY);
+            IsDown = true;
+        }
+
+        if (IsDown && act == 'M') // Flyttar objektet asynkront i musens riktning.
+        {
+            if ((int)e.ClientX - Diff_X > 0) Pos_H = (int)e.ClientX - Diff_X; else Pos_H = 0;
+            if (height - ((int)e.ClientY + Diff_Y) > 0) Pos_V = height - ((int)e.ClientY + Diff_Y); else Pos_V = 0;
+        }
+
+        if (act == 'U') IsDown = false; // När man släpper musknappen.
+    }
 }
